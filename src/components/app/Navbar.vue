3695fd9df57aa11e8711223c93c79e865c984857
Nazar Shcherbyna <shherbina.nazar@gmail.com>
2021-03-02 12:33:07 +0200

    make date

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,7 +5,7 @@
                 <a href="#" @click.prevent="$emit('click')">
                     <i class="material-icons black-text">dehaze</i>
                 </a>
-                <span class="black-text">12.12.12</span>
+                <span class="black-text">{{dateFilter('datetime')}}</span>
                 </div>
 
                 <ul class="right hide-on-small-and-down">
@@ -14,6 +14,7 @@
                         class="dropdown-trigger black-text"
                         href="#"
                         data-target="dropdown"
+                        ref="dropdown"
                     >
                     USER NAME
                     <i class="material-icons right">arrow_drop_down</i>
@@ -21,13 +22,13 @@
 
                     <ul id='dropdown' class='dropdown-content'>
                     <li>
-                        <a href="#" class="black-text">
+                        <router-link to="/profile" class="black-text">
                         <i class="material-icons">account_circle</i>Профиль
-                        </a>
+                        </router-link>
                     </li>
                     <li class="divider" tabindex="-1"></li>
                     <li>
-                        <a href="#" class="black-text">
+                        <a href="#" class="black-text" @click.prevent="logout">
                         <i class="material-icons">assignment_return</i>Выйти
                         </a>
                     </li>
@@ -37,3 +38,49 @@
         </div>
     </nav>
 </template>
+
+<script>
+export default {
+    data: () => ({
+        date: new Date(),
+        interval: null,
+        dropdown: null
+    }),
+    methods: {
+        dateFilter(value, format = 'date') {
+            value = this.date
+
+            const options= {}
+
+            if (format.includes('date')) {
+                options.day = '2-digit'
+                options.month = 'long'
+                options.year = 'numeric'
+                options.hour = '2-digit'
+                options.minute = '2-digit'
+                options.second = '2-digit'
+            }
+
+            return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value))
+        },
+        logout() {
+            console.log('logout');
+            this.$router.push('/login?message=logout')
+        }
+    },
+    mounted() {
+        this.interval = setInterval(() => {
+            this.date = new Date()
+        }, 1000)
+        this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
+            constrainWidth: false
+        });
+    },
+    beforeDestroy() {
+        clearInterval(this.interval)
+        if (this.dropdown && this.dropdown.destroy) {
+            this.dropdown.destroy()
+        }
+    }
+}
+</script>
